ab1c619e290db2dee06b8bfbbd1dc87578c90b5f
Nm1mp <cherepalex04@gmail.com>
2024-12-12 08:59:12 +0100

    SceneUpdate
    -Scene changes
    -Script update

--- Assets/Scripts/CurtainController.cs
+++ Assets/Scripts/CurtainController.cs
@@ -30,6 +30,7 @@ public class CurtainController : MonoBehaviour
         }
         else
         {
+            Debug.Log("working");
             
             leftCurtain.localPosition = Vector3.MoveTowards(leftCurtain.localPosition, leftClosedPosition, moveSpeed * Time.deltaTime);
             rightCurtain.localPosition = Vector3.MoveTowards(rightCurtain.localPosition, rightClosedPosition, moveSpeed * Time.deltaTime);
